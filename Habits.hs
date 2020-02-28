0d3602ef243cc31ba7bdde3229594a47cdc1a041
Tomás Sironi <sironitomas@gmail.com>
2020-02-28 19:30:00 -0300

    Adds habit score calculation

--- Habits.hs
+++ Habits.hs
@@ -5,6 +5,7 @@ import Data.Aeson
 import Data.List
 import Data.Maybe
 import Data.Text
+import Data.Time
 import Data.Time.Calendar
 import GHC.Generics
 import qualified Data.ByteString.Lazy as B
@@ -40,4 +41,18 @@ view [fileName] = do
     -- our choice. In this case, just print it.
     case d of
         Left err -> putStrLn err
-        Right ps -> print ps
+        Right ps -> do
+            today <- getToday
+            print (Prelude.map (getHabitScore today) ps)
+
+getToday :: IO Day
+getToday = utctDay <$> getCurrentTime
+
+getHabitScore :: Day -> Habit -> Int
+getHabitScore today h = score where
+    ds = Prelude.map (getDayScore today) (occurences h)
+    score = round (100 * sum ds)
+
+getDayScore :: Day -> Day -> Float
+getDayScore d1 d2 = ds where
+    ds = 1 / (5 * fromInteger(1 + diffDays d1 d2))
