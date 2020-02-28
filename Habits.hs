88e783f32a9171187a8e444b21a7d9e5ced371b5
Tomás Sironi <sironitomas@gmail.com>
2020-02-28 15:15:00 -0300

    Adds file parsing

--- Habits.hs
+++ Habits.hs
@@ -1,19 +1,43 @@
-import System.Environment
-import System.Directory
-import System.IO
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Aeson
 import Data.List
+import Data.Maybe
+import Data.Text
+import Data.Time.Calendar
+import GHC.Generics
+import qualified Data.ByteString.Lazy as B
+import System.Directory
+import System.Environment
+import System.IO
+
+getJSON :: FilePath -> IO B.ByteString
+getJSON = B.readFile
+
+data Habit = Habit { name :: Text
+                   , occurences :: [Day]
+                   } deriving (Generic, Show)
+
+instance FromJSON Habit
+instance ToJSON Habit
 
 dispatch :: [(String, [String] -> IO ())]
-dispatch =  [ ("add", add)
-            , ("view", view)
-            , ("remove", remove)
-            ]
+dispatch =  [ ("view", view) ]
 
 main = do
     (command:args) <- getArgs
     let (Just action) = lookup command dispatch
     action args
 
-add :: [String] -> IO ()
 view :: [String] -> IO ()
-remove :: [String] -> IO ()
+view [fileName] = do
+    -- Get JSON data and decode it
+    d <- (eitherDecode <$> getJSON fileName) :: IO (Either String [Habit])
+    -- If d is Left, the JSON was malformed.
+    -- In that case, we report the error.
+    -- Otherwise, we perform the operation of
+    -- our choice. In this case, just print it.
+    case d of
+        Left err -> putStrLn err
+        Right ps -> print ps
